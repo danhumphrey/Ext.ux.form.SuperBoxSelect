5863007b25067c14d0f6b8936718357b8047db16
Dan Humphrey <danh2000@gmail.com>
2018-02-18 21:06:44 +1100

    added component and GitHub Pages example

---
+++ docs/states.js
@@ -0,0 +1,70 @@
+/*
+ * 
+ * Ext JS Library 2.2
+ * 
+ * Copyright(c) 2006-2008, Ext JS, LLC.
+ * 
+ * licensing@extjs.com
+ * 
+ * 
+ * 
+ * http://extjs.com/license
+ * 
+ */
+
+       
+
+// some data used in the examples
+Ext.namespace('Ext.exampledata');
+
+Ext.exampledata.states = [['AL', 'Alabama', 'The Heart of Dixie', 'ALClass'],
+		['AK', 'Alaska', 'The Land of the Midnight Sun', 'AKClass'],
+		['AZ', 'Arizona', 'The Grand Canyon State', 'AZClass'],
+		['AR', 'Arkansas', 'The Natural State', 'ARClass'],
+		['CA', 'California', 'The Golden State', 'CAClass'],
+		['CO', 'Colorado', 'The Mountain State', 'COClass'],
+		['CT', 'Connecticut', 'The Constitution State', 'CTClass'],
+		['DE', 'Delaware', 'The First State', 'DEClass'],
+		['DC', 'District of Columbia', "The Nation's Capital", 'DCClass'],
+		['FL', 'Florida', 'The Sunshine State', 'FLClass'],
+		['GA', 'Georgia', 'The Peach State', 'GAClass'],
+		['HI', 'Hawaii', 'The Aloha State', 'HIClass'],
+		['ID', 'Idaho', 'Famous Potatoes', 'IDClass'],
+		['IL', 'Illinois', 'The Prairie State', 'ILClass'],
+		['IN', 'Indiana', 'The Hospitality State', 'INClass'],
+		['IA', 'Iowa', 'The Corn State', 'AIClass'],
+		['KS', 'Kansas', 'The Sunflower State', 'KSClass'],
+		['KY', 'Kentucky', 'The Bluegrass State', 'KYClass'],
+		['LA', 'Louisiana', 'The Bayou State', 'LAClass'],
+		['ME', 'Maine', 'The Pine Tree State', 'MEClass'],
+		['MD', 'Maryland', 'Chesapeake State', 'MDClass'],
+		['MA', 'Massachusetts', 'The Spirit of America', 'MAClass'],
+		['MI', 'Michigan', 'Great Lakes State', 'MIClass'],
+		['MN', 'Minnesota', 'North Star State', 'MNClass'],
+		['MS', 'Mississippi', 'Magnolia State', 'MSClass'],
+		['MO', 'Missouri', 'Show Me State', 'MOClass'],
+		['MT', 'Montana', 'Big Sky Country', 'MTClass'],
+		['NE', 'Nebraska', 'Beef State', 'NEClass'],
+		['NV', 'Nevada', 'Silver State', 'NVClass'],
+		['NH', 'New Hampshire', 'Granite State', 'NHClass'],
+		['NJ', 'New Jersey', 'Garden State', 'NJClass'],
+		['NM', 'New Mexico', 'Land of Enchantment', 'NMClass'],
+		['NY', 'New York', 'Empire State', 'NYClass'],
+		['NC', 'North Carolina', 'First in Freedom', 'NCClass'],
+		['ND', 'North Dakota', 'Peace Garden State', 'NDClass'],
+		['OH', 'Ohio', 'The Heart of it All', 'OHClass'],
+		['OK', 'Oklahoma', 'Oklahoma is OK', 'OKClass'],
+		['OR', 'Oregon', 'Pacific Wonderland', 'ORClass'],
+		['PA', 'Pennsylvania', 'Keystone State', 'PAClass'],
+		['RI', 'Rhode Island', 'Ocean State', 'RIClass'],
+		['SC', 'South Carolina', 'Nothing Could be Finer', 'SCClass'],
+		['SD', 'South Dakota', 'Great Faces, Great Places', 'SDClass'],
+		['TN', 'Tennessee', 'Volunteer State', 'TNClass'],
+		['TX', 'Texas', 'Lone Star State', 'TXClass'],
+		['UT', 'Utah', 'Salt Lake State', 'UTClass'],
+		['VT', 'Vermont', 'Green Mountain State', 'VTClass'],
+		['VA', 'Virginia', 'Mother of States', 'VAClass'],
+		['WA', 'Washington', 'Green Tree State', 'WAClass'],
+		['WV', 'West Virginia', 'Mountain State', 'WVClass'],
+		['WI', 'Wisconsin', "America's Dairyland", 'WIClass'],
+		['WY', 'Wyoming', 'Like No Place on Earth', 'WYClass']];
